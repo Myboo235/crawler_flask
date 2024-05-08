e5fb3787318e826f110be2346533b676795ee950
Hoang Cong Viet <hoangcongviet2003@gmail.com>
2024-05-08 11:40:32 +0700

    "crud"

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from bson.objectid import ObjectId
 import json
 import requests
 from bs4 import BeautifulSoup
+from uuid import uuid1
 
 app = Flask(__name__)
 
@@ -15,19 +16,20 @@ todos = db.todos
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    if request.method=='POST':
-        content = request.form['content']
-        degree = request.form['degree']
-        todos.insert_one({'content': content, 'degree': degree})
-        return redirect(url_for('index'))
+    # if request.method=='POST':
+    #     content = request.form['content']
+    #     degree = request.form['degree']
+    #     todos.insert_one({'content': content, 'degree': degree})
+    #     return redirect(url_for('index'))
 
-    all_todos = todos.find()
-    return render_template('index.html', todos=all_todos)
+    # all_todos = todos.find()
+    # return render_template('index.html', todos=all_todos)
+    return crawl_data()
 
-@app.post('/<id>/delete/')
-def delete(id):
-    todos.delete_one({"_id": ObjectId(id)})
-    return redirect(url_for('index'))
+# @app.post('/<id>/delete/')
+# def delete(id):
+#     todos.delete_one({"_id": ObjectId(id)})
+#     return redirect(url_for('index'))
 
 
 @app.route('/crawl')
@@ -73,8 +75,8 @@ def crawl():
                 # print("Answer: ", answers[j+sum])
             sum += int(topic_counts[topics[i].text])
 
-        qa_pairs = [{"id": i, "question": question, "answer": answer, "topic": topic} for i, (question, answer, topic) in enumerate(zip( questions, answers, topic_elements))]
-
+        #qa_pairs = [{"id": i, "question": question, "answer": answer, "topic": topic} for i, (question, answer, topic) in enumerate(zip( questions, answers, topic_elements))]
+        qa_pairs = [{"question": question, "answer": answer, "topic": topic} for question, answer, topic in zip(questions, answers, topic_elements)]
 
 
         db = client['crawl']  # replace with your database name
@@ -91,10 +93,82 @@ def crawl_data():
     
     db = client['crawl']
     collection = db['crawl_data']
-    data = collection.find({}, {"_id": 0})  # Exclude _id
+    data = collection.find() 
 
-    # Convert the cursor object to a list
+
     data_list = list(data)
 
-    # Convert the list to a JSON object and return it withou sorting the keys (default)
+    
     return jsonify(data_list)
+
+@app.post('/create')
+def insert_data():
+    db = client['crawl']
+    collection = db['crawl_data']
+
+    _id = str(uuid1().hex)
+
+
+    content = dict(request.json)
+    content.update({"_id": _id})
+
+    result = collection.insert_one(content)
+    if not result.inserted_id:
+        return {"message": "Failed to insert data"}, 500
+    
+    return {
+        "message": "Success",
+        "data": {
+            "_id": result.inserted_id
+        }
+    }, 200
+
+@app.post('/update')
+def update_data():
+    db = client['crawl']
+    collection = db['crawl_data']
+
+    _id = request.json.get("id")
+
+    content = dict(request.json)
+    content.pop("id")
+
+    result = collection.update_one({"_id": _id}, {"$set": content})
+    if not result.modified_count:
+        return {"message": "Failed to update data"}, 500
+    
+    return {
+        "message": "Success"
+    }, 200
+
+
+@app.post('/delete')   
+def delete():
+    db = client['crawl']
+    collection = db['crawl_data']
+    _id = request.json.get("id")
+
+ 
+    result = collection.delete_one({"_id": ObjectId(_id)})
+
+
+    if not result.deleted_count:
+        return {"message": "Failed to delete data"}, 500
+    
+    return {
+        "message": "Success"
+    }, 200
+
+
+
+
+
+
+
+
+    
+
+    
+
+    
+    
